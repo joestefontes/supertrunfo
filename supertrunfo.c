6a41c7ecedcb3ff48b8e8d312383fe55f44e1e06
joestefontes <71931442+joestefontes@users.noreply.github.com>
2025-04-10 13:20:15 -0300

    Add files via upload

---
+++ supertrunfo.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+
+int main()
+{
+int População1,População2;
+int Turismo1,Turismo2;
+char Carta1[10],Carta2[10];
+float Área1,Área2;
+float Pib1,Pib2;
+char nome[20];
+char Estado1[10],Estado2[10];
+char Cidade1[10],Cidade2[10];
+char Código1[10],Código2[10];
+
+
+printf("Digíte o número da carta:\n ");
+scanf("%s", &Carta1);
+
+printf("Digíte o Estado:\n");
+scanf("%s",&Estado1);
+
+printf("Digíte o código do Estado:\n");
+scanf("%s",&Código1);
+
+printf("Digíte o nome da cidade:\n");
+scanf("%s",&Cidade1);
+
+printf("Digíte a população habitacional:\n");
+scanf("%d", &População1);
+
+printf("Digite a área:\n");
+scanf("%f", &Área1);
+printf("Digíte o PIB da cidade:\n");
+scanf("%f", &Pib1);
+
+printf("Digíte o número de pontos turísticos:\n");
+scanf("%d", &Turismo1);
+
+printf("Digíte o número da carta:\n ");
+scanf("%s", &Carta2);
+
+printf("Digíte o Estado:\n");
+scanf("%s",&Estado2);
+
+printf("Digíte o código do Estado:\n");
+scanf("%s",&Código2);
+
+printf("Digíte o nome da cidade:\n");
+scanf("%s",&Cidade2);
+
+printf("Digíte a população habitacional:\n");
+scanf("%d", &População2);
+
+printf("Digite a área:\n");
+scanf("%f", &Área2);
+
+printf("Digíte o PIB da cidade:\n");
+scanf("%f", &Pib2);
+
+printf("Digíte o número de pontos turísticos:\n");
+scanf("%d", &Turismo2);
+
+printf("Carta:%s\n", Carta1);
+printf("Estado:%s\n", Estado1);
+printf("Código:%s\n", Código1);
+printf("Cidade:%s\n", Cidade1);
+printf("População:%d\n", População1);
+printf("Área:%f\n", Área1);
+printf("PIB:%f\n", Pib1);
+printf("Números de Pontos Turísticos:%d\n", Turismo1);
+
+printf("Carta:%s\n", Carta2);
+printf("Estado:%s\n", Estado2);
+printf("Código:%s\n", Código2);
+printf("Cidade:%s\n", Cidade2);
+printf("População:%d\n", População2);
+printf("Área:%f\n", Área2);
+printf("PIB:%f\n", Pib2);
+printf("Números de Pontos Turísticos:%d\n", Turismo2);
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+
+
+
+}
+
+
+
+
